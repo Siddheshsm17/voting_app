abeff018fc08e0309d3387604da238ca1289ce0f
LORD SM <siddhesh@example.com>
2025-01-29 01:35:29 +0530

    file added

---
+++ db.js
@@ -0,0 +1,33 @@
+
+const mongoose= require('mongoose');
+// define mongo db connectoin url
+const mongoURL='mongodb://localhost:27017/hotels'; 
+//const mongoURL='mongodb+srv://siddheshsm19:mongocluster190@cluster0.uhuk8.mongodb.net/';
+//const mongoURL = 'mongodb+srv://siddheshsm19:mongocluster190@cluster0.uhuk8.mongodb.net/<dbname>?retryWrites=true&w=majority&ssl=true';
+//const mongoURL=process.env.MONGODB_URL_LOCAL;
+ 
+console.log(mongoURL);
+
+// set up mongo db connection ong
+mongoose.connect(mongoURL)
+
+// get default mongodb connection 
+// mongoose maintain  a default connection object representing the Mongo DB
+const db = mongoose.connection; 
+// this object will handle event and interact with user 
+
+// define event listners for database connection 
+
+
+db.on( 'connected', ()=>{
+    console.log('connected to mongodb serverrr')
+});
+
+db.on('error', (err)=>{
+  console.log('mongo db connectiom error :', err)
+})
+
+db.on('disconnected',()=>{
+    console.log('mongodb is disconnected')
+});
+
